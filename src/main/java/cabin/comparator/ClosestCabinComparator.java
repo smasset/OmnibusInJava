7dafffcbc9f4291568ca866522a705532e8ee132
Sébastien Masset <smasset@server>
2013-12-12 07:51:35 +0100

    Fixed ClosestCabinComparator

--- src/main/java/cabin/comparator/ClosestCabinComparator.java
+++ src/main/java/cabin/comparator/ClosestCabinComparator.java
@@ -1,18 +1,29 @@
 package cabin.comparator;
 
+import java.util.Comparator;
+
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
 import cabin.util.Cabin;
 import cabin.util.FloorRequest;
 
-public class ClosestCabinComparator extends AbstractCabinComparator {
+public class ClosestCabinComparator implements Comparator<Cabin> {
+	protected FloorRequest request = null;
 
 	public ClosestCabinComparator(FloorRequest request) {
-		super(request);
+		this.request = request;
+	}
+
+	protected Integer getDistance(Cabin cabin) {
+		return this.request.getAbsoluteDistance(cabin.getCurrentFloor());
 	}
 
 	@Override
-	protected Double getScore(Cabin cabin) {
-		Double score = this.request.getAbsoluteDistance(cabin.getCurrentFloor()).doubleValue();
-		return score;
+	public int compare(Cabin o1, Cabin o2) {
+		return new CompareToBuilder()
+				.append(this.getDistance(o1), this.getDistance(o2))
+				.append(o1.getPopulation(), o2.getPopulation())
+				.append(o1.getId(), o2.getId()).toComparison();
 	}
 
 }
